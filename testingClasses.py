dcbc7e778a724bbd2c048fdfb5f35b2bc39286b8
glebdubinin <glebka090107@gmail.com>
2024-12-21 18:13:29 +1030

    made progress in inventory management, need to convert movement between rooms and spaces to be based around IDs rather than recursively calling the object itself.

--- testingClasses.py
+++ testingClasses.py
@@ -1,10 +1,19 @@
 class Container():
     def __init__(self, parent, neighbors, name):
+        self.locID = None
         self.parent = parent
         self.neighbors = neighbors # neighboring places and rooms that can be entered
         self.items = {}
         self.name = name
 
 class Player():
-    def __init__(self, location):
-        self.location = location
+    def __init__(self):
+        self.name = ""
+        self.location = None
+        self.inventory = {}
+        self.inventoryExpander = None # the object that defines the player's current inventory capacity
+
+class Object():
+    def __init__(self, name):
+        self.name = name
+        self.space = None
--- testingMain.py
+++ testingMain.py
@@ -1,4 +1,6 @@
 from testingClasses import *
+from testingFunctions import *
+from datetime import date
 
 ## initialisation; delete later
 
@@ -7,46 +9,8 @@ bedroom = Container(parent = None, neighbors={livingroom}, name = "bedroom")
 
 livingroom.neighbors = {bedroom}
 
-player = Player(location = bedroom)
-
-commands = {"go" : {"go", "goto", "move", "moveto"},
-            "grab" : {"grab", "pickup", "take", "yoink", "snatch", "snag", "grasp", "snatch"},
-            "drop" : {"drop", "put down", "leave", "leave behind"},
-            "use" : {"use", "apply", "utilise", "check"},
-            "exit" : {"exit"},
-            "look" : {"look", "seek", "search", "seek", "look", "gaze", "glance", "stare", "peer", "survey", "scan", "peruse"}}
-
-def getInput(line):
-    quote = input(line)+" "
-    usermove = []
-    placeholder = ""
-    for i in range(len(quote)):
-        if quote[i] == " ":
-            usermove.append(placeholder)
-            placeholder = ""
-        else:
-            placeholder += quote[i]
-    try:
-        commandValid = False
-        for command in commands:
-            if usermove[0] in commands[command]:
-                commandValid = True
-        if not commandValid:
-            print("that ain't a command, retard")
-    except IndexError:
-        pass
-    return usermove
-
-def getRoomNeighbors(room):
-    neighbors = []
-    for i in room.neighbors:
-        neighbors.append(i)
-    return neighbors
-
-def goto(placefrom, placeto):
-    if placeto in getRoomNeighbors(placefrom):
-        player.location = placeto
-
+player = Player()
+player.location = bedroom
 
 def main():
     print("executing def main")
@@ -54,13 +18,17 @@ def main():
     while playing:
         usermove = getInput("What would you like to do next?  ")
         #print(usermove)
+
         if usermove[0] in commands["go"]:
-            for neighbor in getRoomNeighbors(player.location):
-                if usermove[1] == neighbor.name:
-                    player.location = neighbor
-                    print("You have moved to the " + neighbor.name)
-        elif usermove[0] == "exit":
+            goto(placeto=usermove[1], player=player)
+            #for neighbor in getRoomNeighbors(player.location):
+            #    if usermove[1] == neighbor.name:
+            #        player.location = neighbor
+            #        print("You have moved to the " + neighbor.name)
+
+        elif usermove[0] in commands["exit"]:
             playing = False
+
         elif usermove[0] in commands["look"]:
             print(f"You are in the {player.location.name}")
             print("You can go to:")
@@ -70,6 +38,23 @@ def main():
                 print("And you can see:")
                 for item in player.location.items:
                     print(f" - {item.name}")
+
+        elif usermove[0] in commands["grab"]:
+            for item in player.location.items:
+                if usermove[1] == item.name:
+                    if item.space <= getFreeInvCapacity(player):
+                        player.inventory.append(item)
+                        player.location.items.remove(item)
+                        print(f"You picked up {item.name}")
         
+        elif usermove[0] == "save":
+            gamedata = {
+                "player" : {
+                    "lastSaved" : str(date.today()),
+                    "name" : player.name,
+                    "location" : str(player.location)
+                }
+            }
+            pass
 
 main()
